87ef5aa3f6619adb8d80df02391f9a36692a132f
Tim885885 <Tim885885@users.noreply.github.com>
2023-05-11 16:06:45 +0800

    Update app.js

--- app.js
+++ app.js
@@ -8,8 +8,32 @@ var belaboxStatssensor = "";
 var lusStats = "";
 
 socket = new WebSocket("wss://remote.belabox.net/ws/remote")
+
+//Enter your Belabox remote cloud key
 var key = "***KEY HERE***"
 
+//Network Status Details
+var eth0 = "Chunghwa Telecom"
+var eth1 = "eth1"
+var eth2 = "eth2"
+var eth3 = "eth3"
+var eth4 = "eth4"
+var wlan0 = "Far EasTone"
+var wlan1 = "wlan1"
+var wlan2 = "wlan2"
+var wlan3 = "wlan3"
+var wlan4 = "wlan4"
+
+//Some for localization
+var totalspeed = "Total bitrate"
+var Soctemperature = "Temperature"
+var networkstatusname = "Network Status"
+
+//Some options
+var fileupdatetime = 2000;  //File Update Interval min is 1500, 1.5sec, obs's file loading interval is 2 seconds, so 2000 is recommended
+var totalspeedison = true; //Show total bitrate (Not completed)
+var tempison = true; //Display temperature (Not completed)
+var oneline = true; //Display text as a line, not a list (Not completed)
 
 function connect() {
     socket.onopen = function () {
@@ -36,13 +60,47 @@ function connect() {
 function genNetifEntry(error, enabled, name, ip, throughput, isBold = false) {
     let entry;
     let state = "";
-
-    if (enabled) {
-        state = "On";
-    } else {
-        state = "Off";
-    }
-    entry = (!isBold ? name : 'Total bitrate') + ': ' + throughput + (!isBold ? ' (' + state + ').' : '');
+	
+	if (name == "eth0") {
+		name = eth0;
+	}
+	
+	if (name == "eth1") {
+		name = eth1;
+	}
+	
+	if (name == "eth2") {
+		name = eth2;
+	}
+	
+	if (name == "eth3") {
+		name = eth3;
+	}
+	if (name == "eth4") {
+		name = eth4;
+	}
+	
+	if (name == "wlan0") {
+		name = wlan0;
+	}
+	
+	if (name == "wlan1") {
+		name = wlan1;
+	}
+	
+	if (name == "wlan2") {
+		name = wlan2;
+	}
+	
+	if (name == "wlan3") {
+		name = wlan3;
+	}
+	
+	if (name == "wlan4") {
+		name = wlan4;
+	}
+	
+    entry = (!isBold ? name : totalspeed) + ': ' + throughput + (!isBold ? '' : '');
     if (error) {
         console.log("Belabox: " + error);
     }
@@ -59,12 +117,12 @@ function updateNetif(netifs) {
         data = netifs[i];
         tpKbps = Math.round((data['tp'] * 8) / 1024);
         totalKbps += tpKbps;
-        modemList.push(genNetifEntry(data.error, data.enabled, i, data.ip, `${tpKbps} Kbps`));
+        modemList.push(genNetifEntry(data.error, data.enabled, i, data.ip, `${tpKbps} Kbps\n`));
     }
     if (Object.keys(netifs).length > 1) {
         modemList.push(genNetifEntry(undefined, undefined, '', '', `${totalKbps} Kbps`, true));
     }
-    belaboxStatsNet = modemList.join(" ");
+    belaboxStatsNet = modemList.join("");
 }
 
 function updateSensors(sensors) {
@@ -73,7 +131,7 @@ function updateSensors(sensors) {
 
     for (const i in sensors) {
         dataS = sensors[i];
-        sensorList.push(i + ' ' + dataS);
+        sensorList.push('\n' + Soctemperature + ': ' + dataS);
     }
     belaboxStatssensor = sensorList.join(" ");
 }
@@ -122,37 +180,31 @@ function handleMessageBB(msg) {
 function connectSetup() {
     var belaboxFileUpdate;
     connect();
-    setTimeout(() => {
+    setInterval(() => {
         if (belaboxStatsState) {
             if (!belaboxStatsNet == "") {
-                lusStats = belaboxStatsNet + '. ';
+                lusStats = belaboxStatsNet + '';
             }
             if (!belaboxStatssensor == "") {
                 lusStats = lusStats + belaboxStatssensor;
             }
-            belaboxFileUpdate = 'Belabox stats: ' + lusStats;
-            console.log('');
-            console.log(belaboxFileUpdate);
-            console.log('Belabox: file belaboxstats.txt updated in this folder');
+            belaboxFileUpdate = networkstatusname + ':\n' + lusStats;
+            console.log(belaboxFileUpdate + '\n');
             fs.writeFile('belaboxstats.txt', belaboxFileUpdate, (err) => {
                 // In case of a error throw err. 
                 if (err) throw err;
             })
-            console.log('');
         } else {
             belaboxFileUpdate = 'Belabox: connection offline!';
-            console.log(belaboxFileUpdate);
-            console.log('Belabox: file belaboxstats.txt updated in this folder');
-            console.log('');
+            console.log(belaboxFileUpdate + '\n');
             fs.writeFile('belaboxstats.txt', belaboxFileUpdate, (err) => {
                 // In case of a error throw err. 
                 if (err) throw err;
             })
         }
-        //clean up in case
         socket.send(JSON.stringify({ logout: true }));
-        socket.close();
-    }, 4000);
+        
+    }, fileupdatetime);
 
 }
 
